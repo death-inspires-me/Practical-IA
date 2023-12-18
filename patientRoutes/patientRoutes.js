ec9c1dc4e8d56bc512cc56c1988b8e7fbcb9287d
death-inspires-me <nana.esseney@gmail.com>
2023-12-18 09:27:41 +0000

    Patient registration functionality done. Fixing bugs(Internal Server Error)

--- patientRoutes/patientRoutes.js
+++ patientRoutes/patientRoutes.js
@@ -20,7 +20,7 @@ router.post('/register', async (req, res) => {
 
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
